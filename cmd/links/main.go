b5629a12dabe2339f4e3bace4533047932b70d10
Sijis Aviles <sijis.aviles@gmail.com>
2017-09-12 09:37:38 -0500

    refactor: Simplify struct and strip out endpoint

--- cmd/links/main.go
+++ cmd/links/main.go
@@ -10,9 +10,12 @@ import (
 )
 
 type webData struct {
-    endpoint string
-    dataset map[string]string
+    dataset  map[string]string
 }
+
+var (
+    endpoint = "http://localhost:5000/locker"
+)
 
 func main() {
     user, _ := user.Current()
@@ -20,30 +23,32 @@ func main() {
     action := flag.String("action", "locked", "Action to take.")
     chest := flag.String("chest", "", "Which chest to lock/unlock.")
     flag.Parse()
-    dataset := map[string]string{"username": "@"+*name, "action": *action, "chest": *chest}
 
-    w := webData{"/locker", dataset}
+    dataset := map[string]string{
+        "username": "@" + *name,
+        "action":   *action,
+        "chest":    *chest,
+    }
+
+    w := webData{dataset}
     fmt.Println(postToWeb(w))
 }
 
 func postToWeb(data webData) string {
 
     fmt.Println("[DEBUG] postToWeb: ", data)
-    _url := "http://localhost:5000" + data.endpoint
-
     params := url.Values{}
     for k, v := range data.dataset {
         params.Add(k, v)
     }
 
-    resp, err := http.PostForm(_url, params)
+    resp, err := http.PostForm(endpoint, params)
 
     if err != nil {
-        //fmt.Println("Error: ", err)
         panic(err)
     }
 
     defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
+    body, _ := ioutil.ReadAll(resp.Body)
     return string(body)
 }
